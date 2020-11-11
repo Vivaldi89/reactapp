65eb623df7e1711b2a038459a7834a3c59b449ff
qwe <qwe@qwe.com>
2020-11-11 08:29:53 +0000

    dsf

--- src/app/store.js
+++ src/app/store.js
@@ -2,8 +2,15 @@ import { configureStore } from '@reduxjs/toolkit';
 import todoSlice from './Containers/TodoList/todoSlice';
 
 
-export default configureStore({
-  reducer: todoSlice.reducer
-    // todo: //todo link here todo reducer from the slice
+// export default configureStore({
+//   reducer: todoSlice.reducer
+//     // todo: //todo link here todo reducer from the slice
   
+// });
+
+
+const store = configureStore({
+  reducer: todoSlice.reducers
 });
+
+export default store
